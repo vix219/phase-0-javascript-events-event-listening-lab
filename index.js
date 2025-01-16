822aeee08a08cc71057e7f431e16ceae9b7a780b
VictoriaCarvalho <vix219@icloud.com>
2025-01-16 10:21:45 -0600

    add lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    input.addEventListener('click', clickAlert);
 }
+const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
